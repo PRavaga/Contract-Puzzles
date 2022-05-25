1d4329801de3094dce662fb37842e57365f95371
PRavaga <trueravaga@gmail.com>
2022-05-25 22:06:25 +0200

    solutions

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,14 +1,24 @@
 const { assert } = require("chai");
 
-describe("Game4", function() {
-  it("should be a winner", async function() {
+describe("Game4", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
     await game.deployed();
 
     // nested mappings are rough :}
+    // this works but probably not what we're looking for
+    // const signer = ethers.provider.getSigner(0);
+    // const address = await signer.getAddress();
+    // await game.write(address);
+    // await game.win(address);
 
-    await game.win();
+    const signer0 = ethers.provider.getSigner(0);
+    const address0 = await signer0.getAddress();
+    const signer1 = ethers.provider.getSigner(1);
+    const address1 = await signer1.getAddress();
+    await game.connect(signer0).write(address1);
+    await game.connect(signer1).win(address0);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,14 +1,28 @@
 const { assert } = require("chai");
 
-describe("Game5", function() {
-  it("should be a winner", async function() {
+describe("Game5", function () {
+  it("should be a winner", async function () {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    const threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
 
-    await game.win();
+    let wallet = ethers.Wallet.createRandom();
+
+    const generateAddress = async () => {
+      while (wallet.address > threshold) {
+        wallet = ethers.Wallet.createRandom();
+      }
+      const signer = ethers.provider.getSigner(0);
+      await signer.sendTransaction({
+        to: wallet.address,
+        value: ethers.utils.parseUnits("1", "ether"),
+      });
+      return new ethers.Wallet(wallet.privateKey, ethers.provider);
+    };
+
+    await game.connect(await generateAddress()).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
